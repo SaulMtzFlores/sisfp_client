ddd659a0c812c42a0f5911bb8ed1d6bf6c731ece
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-10-07 16:07:34 -0500

    Deployment work

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -17,5 +17,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent }
+    { path: 'upgrade',        component: UpgradeComponent },
+    { path: '**', pathMatch:'full', redirectTo: 'dashboard'}
 ];
