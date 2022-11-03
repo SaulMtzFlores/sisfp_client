47ae889823c2cbd1a6279e4d2861b6e96390b272
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-02 18:22:45 -0600

    Auth components updt 2

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,14 +1,12 @@
 import { Routes } from "@angular/router";
 import { RecoverComponent } from "app/modules/auth/recover/recover.component";
 import { RedeemComponent } from "app/modules/auth/redeem/redeem.component";
-import { ResetPasswordComponent } from "app/modules/auth/reset-password/reset-password.component";
 import { SignInComponent } from "app/modules/auth/sign-in/sign-in.component";
 import { SignUpComponent } from "app/modules/auth/sign-up/sign-up.component";
 
 export const AuthLayoutRoutes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'redeem', component: RedeemComponent },
   { path: 'recover', component: RecoverComponent },
   { path: '**', pathMatch:'full', redirectTo: 'sign-in'}
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RedeemComponent } from './redeem/redeem.component';
 import { RecoverComponent } from './recover/recover.component';
@@ -12,7 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     SignUpComponent,
-    ResetPasswordComponent,
     SignInComponent,
     RedeemComponent,
     RecoverComponent
