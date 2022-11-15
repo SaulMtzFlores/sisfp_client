24284611a7eb43a923244e6d79694957e05b12c1
saulmartinez.llevele <saulmartinez.llevele@gmail.com>
2022-11-14 18:58:56 -0600

    Posts advance

--- src/app/modules/groups/post-view/post-view.component.ts
+++ src/app/modules/groups/post-view/post-view.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiProvider } from 'app/providers/api';
+import { DefaultLoaderService } from 'app/providers/default-loader';
+import { NotificationsService } from 'app/providers/notifications';
+import { TokenService } from 'app/providers/token';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'post-view',
@@ -7,9 +13,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostViewComponent implements OnInit {
 
-  constructor() { }
+  resourceName = 'groups';
+  subresourceName = 'posts';
 
-  ngOnInit(): void {
+  loading: boolean = false;
+
+  listenerParams: Subscription;
+
+  groupId: any;
+  modelId: any;
+
+  model: any;
+
+  allowEdit:boolean = false;
+
+  constructor(
+    private apiProvider: ApiProvider,
+    private router: Router,
+    private notif: NotificationsService,
+    private activatedRoute: ActivatedRoute,
+    private detector: ChangeDetectorRef,
+    private defaultLoader: DefaultLoaderService,
+    private tokenService: TokenService
+  ) { }
+
+  async ngOnInit(): Promise<any> {
+    try {
+      this.loading = true;
+      await new Promise((resolve) => {
+        this.listenerParams = this.activatedRoute.params.subscribe((params) => {
+          if(params.id){
+            this.groupId = params.groupId;
+            this.modelId = params.id;
+          }
+          return resolve(true)
+        })
+      });
+
+      this.model = await this.loadModel();
+      await this.loadUser();
+
+      this.loading = false;
+      this.detector.detectChanges();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loadModel(): Promise<any>{
+    try {
+      const model = await this.apiProvider.get({
+        url: `/${this.resourceName}/${this.groupId}/posts/${this.modelId}`,
+        auth: true
+      });
+      return model;
+    } catch (error) {
+      console.log(error);
+      this.notif.pop('error', 'Lo sentimos, el recurso solicitado no existe');
+      this.router.navigate([`/udg/${this.resourceName}`]);
+    }
+  }
+
+  private async loadUser():Promise<any>{
+    try {
+      console.log(this.tokenService.token);
+      const r = await this.apiProvider.get({
+        url: `/users/${this.tokenService.userId()}`,
+        auth: true
+      });
+      if(r.roleId !== this.defaultLoader.idp('studentRole')){
+        this.allowEdit = true;
+       }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
--- src/app/modules/groups/posts/posts.component.ts
+++ src/app/modules/groups/posts/posts.component.ts
@@ -1,4 +1,12 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiProvider } from 'app/providers/api';
+import { DefaultLoaderService } from 'app/providers/default-loader';
+import { NotificationsService } from 'app/providers/notifications';
+import { TokenService } from 'app/providers/token';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'posts',
@@ -7,9 +15,126 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  resourceName = 'groups';
+  subresourceName = 'posts';
+  models:any = []
+  form: FormGroup;
 
-  ngOnInit(): void {
+  paginationPage: number = 1;
+  paginationPerPage: number = 5;
+
+  loading: boolean = false;
+  allowEdit:boolean = false;
+
+  groupId: any;
+  listenerParams: Subscription;
+
+  constructor(
+    private apiProvider: ApiProvider,
+    private router: Router,
+    private notif: NotificationsService,
+    private defaultLoader: DefaultLoaderService,
+    private tokenService: TokenService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  async ngOnInit(): Promise<any> {
+    try {
+      this.loading = true;
+      await new Promise((resolve) => {
+        this.listenerParams = this.activatedRoute.params.subscribe((params) => {
+          if(params.groupId){
+            this.groupId = params.groupId;
+          }
+          return resolve(true);
+        });
+      });
+
+      this.form = new FormGroup({
+        search: new FormControl(null),
+      });
+      await this.loadModel();
+      await this.loadUser();
+      this.loading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loadModel(): Promise<any>{
+    try {
+      this.loading = true;
+      const searchData = this.form.value;
+
+      const params:any = {
+        paginationPage: this.paginationPage,
+        paginationPerPage: this.paginationPerPage
+      }
+
+      if(searchData.search){
+        params.search = searchData.search
+      }
+
+      const queryParamsString = new HttpParams({
+        fromObject: params,
+      }).toString();
+
+      console.log(`/${this.resourceName}/${this.groupId}/${this.subresourceName}?${queryParamsString}`);
+      const req = await this.apiProvider.get({
+        url: `/${this.resourceName}/${this.groupId}/${this.subresourceName}?${queryParamsString}`,
+        params,
+        auth: true,
+      });
+      this.models = req.data;
+      this.loading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private async loadUser():Promise<any>{
+    try {
+      console.log(this.tokenService.token);
+      const r = await this.apiProvider.get({
+        url: `/users/${this.tokenService.userId()}`,
+        auth: true
+      });
+      if(r.roleId !== this.defaultLoader.idp('studentRole')){
+        this.allowEdit = true;
+       }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async search():Promise<any>{
+    try {
+      const searchData = this.form.value;
+      if(!searchData.search){
+        this.notif.pop('error', 'Búsqueda vacía.');
+      }
+      await this.loadModel();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async setPaginationAmount(amount:number){
+    this.paginationPerPage = amount;
+    await this.loadModel();
+  }
+
+  async setPaginationPage(page:number){
+    if(page <= 0){
+      this.notif.pop('error', 'Página negativa.');
+      return;
+    }
+    this.paginationPage = page;
+    await this.loadModel();
+  }
+
+  routerGo(_id:string){
+    this.router.navigate([`/udg/${this.resourceName}/${this.groupId}/posts/see/${_id}`]);
   }
 
 }
